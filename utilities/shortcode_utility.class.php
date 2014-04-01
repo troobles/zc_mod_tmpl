c25ac9876d7735537aff4484d6b474bb2fc18373
troobles <troobles@users.noreply.github.com>
2014-04-01 15:43:59 -0500

    Create shortcode_utility.class.php

---
+++ utilities/shortcode_utility.class.php
@@ -0,0 +1,23 @@
+<?php
+/*
+ * Class Name: ZC_SHORTCODE_UTILITY
+ * Description: Parent Class for registering of Shortcodes
+ * Version: 1.0
+ * Author: Troobles
+ *
+ * @since x.x.x (if available)
+ * @package zotpress/[this_plugin_name]
+ * 
+ */
+ 
+if( ! class_exists( 'ZC_SHORTCODE_UTILITY' ) ) {
+
+  class ZC_SHORTCODE_UTILITY {
+  
+  
+  
+  
+  
+  } // END CLASS
+
+}
