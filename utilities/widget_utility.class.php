d9b28b33453616fc83be23080e477916024dc004
troobles <troobles@users.noreply.github.com>
2014-04-01 15:41:46 -0500

    Create widget_utility.class.php

---
+++ utilities/widget_utility.class.php
@@ -0,0 +1,23 @@
+<?php
+/*
+ * Class Name: ZC_WIDGET_UTILITY
+ * Description: Template for creating WP Widgets
+ * Version: 1.0
+ * Author: Troobles
+ *
+ * @since x.x.x (if available)
+ * @package zotpress/[this_plugin_name]
+ * 
+ */
+ 
+if( ! class_exists( 'ZC_WIDGET_UTILITY' ) ) {
+
+class ZC_WIDGET_UTILITY extends WP_WIDGET {
+
+
+
+
+
+} // END CLASS
+
+}
