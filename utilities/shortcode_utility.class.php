8a9f68be938c269bd5fa68a95c8ccbbd477ee827
troobles <troobles@users.noreply.github.com>
2014-04-01 17:59:09 -0500

    Update shortcode_utility.class.php

--- utilities/shortcode_utility.class.php
+++ utilities/shortcode_utility.class.php
@@ -10,6 +10,23 @@
  * 
  */
  
+ /*
+  * Related Functions
+  * Function: do_shortcode()
+  * Function: add_shortcode()
+  * Function: remove_shortcode()
+  * Function: remove_all_shortcodes()
+  * Function: shortcode_atts()
+  * Function: strip_shortcodes()
+  * Function: shortcode_exists()
+  * Function: has_shortcode()
+  * Function: get_shortcode_regex()
+  * Function: wp_audio_shortcode()
+  * Function: wp_video_shortcode()
+  * Filter: no_texturize_shortcodes
+  *
+  */
+ 
 if( ! class_exists( 'ZC_SHORTCODE_UTILITY' ) ) {
 
   class ZC_SHORTCODE_UTILITY {
