a09f290eb32e2b90c7f9f516029ecf149b058bdd
troobles <troobles@users.noreply.github.com>
2014-04-01 15:46:02 -0500

    Create taxonomy_utility.class.php

---
+++ utilities/taxonomy_utility.class.php
@@ -0,0 +1,23 @@
+<?php
+/*
+ * Class Name: ZC_TAXONOMY_UTILITY
+ * Description: Parent Class for registering of Taxonomies
+ * Version: 1.0
+ * Author: Troobles
+ *
+ * @since x.x.x (if available)
+ * @package zotpress/[this_plugin_name]
+ * 
+ */
+ 
+if( ! class_exists( 'ZC_TAXONOMY_UTILITY' ) ) {
+
+  class ZC_TAXONOMY_UTILITY {
+  
+  
+  
+  
+  
+  } // END CLASS
+
+}
